6000b04482fd248571aa1875a5fb04be38bed497
rcode125 <wht8465@abwesend.de>
2026-01-16 20:26:00 +0100

    Add more readable

--- JS/main.js
+++ JS/main.js
@@ -8,15 +8,15 @@ function search(keyword) {
     for (const website of db1) {
         if(website[0].toLowerCase().includes(keyword.toLowerCase()) == true) {
             /* Test if the keyword in the Name of the website */
-            console.log(website[0]); /* Print out the Name of the Website. */
-            console.log(website[1]); /* Print out the discription of the website. */
+            console.log(`Name: ${website[0]}`);; /* Print out the name of the website. */
+            console.log(`Discription: ${website[1]}`); /* Print out the discription of the website. */
         }
         if(website[1].toLowerCase().includes(keyword.toLowerCase()) == true) {
             /* Test if the keyword in the Discription */
-            console.log(website[0]); /* Print out the name of the website. */
-            console.log(website[1]); /* Print out the discription of the website. */
+            console.log(`Name: ${website[0]}`);; /* Print out the name of the website. */
+            console.log(`Discription: ${website[1]}`); /* Print out the discription of the website. */
         }
     }
 }
 
-search("Portal");
+search("bu");
