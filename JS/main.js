19b1d05c070cbd4397eb0d6a33da3685c4cf0d04
rcode125 <wht8465@abwesend.de>
2026-01-16 19:54:20 +0100

    Add more readable to main.js
    Add comments and split one if statment to two statements.

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,4 @@
+/* Add a json db to the Programm (loading) */
 const db1 = [
     ["GitHub.com", "Where the word builds software"],
     ["YouTube.com", "Youtube, das Video Portal."]
@@ -5,8 +6,15 @@ const db1 = [
  
 function search(keyword) {
     for (const website of db1) {
-        if(website[0].toLowerCase().includes(keyword.toLowerCase()) == true || website[1].toLowerCase().includes(keyword.toLowerCase()) == true) {
-            console.log(website[0]);
+        if(website[0].toLowerCase().includes(keyword.toLowerCase()) == true) {
+            /* Test if the keyword in the Name of the website */
+            console.log(website[0]); /* Print out the Name of the Website. */
+            console.log(website[1]); /* Print out the discription of the website. */
+        }
+        if(website[1].toLowerCase().includes(keyword.toLowerCase()) == true) {
+            /* Test if the keyword in the Discription */
+            console.log(website[0]); /* Print out the name of the website. */
+            console.log(website[1]); /* Print out the discription of the website. */
         }
     }
 }
