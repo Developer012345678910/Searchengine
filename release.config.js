81212f753a8c341e86bd8bea744dfa51abc226af
rcode125 <wht8465@abwesend.de>
2026-01-26 14:13:04 +0100

    Update GitHub plugin configuration in release.config.js

--- release.config.js
+++ release.config.js
@@ -3,6 +3,12 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/github"
+    [
+      "@semantic-release/github",
+      {
+        success: false,
+        fail: false
+      }
+    ]
   ]
 }
